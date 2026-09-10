54d9d911fd82edf2fd80fc3f7d4ba349a11f66cc
alessiapicciuto <picciutoalessia@gmail.com>
2026-09-10 10:50:15 +0200

    correzione progfilo page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -45,6 +45,7 @@ export default function ProfilePage() {
       return;
     }
     caricaViaggi();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser, navigate]);
 
   function eseguiLogout() {
